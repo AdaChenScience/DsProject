4b9b9f7e1da1ce27b34b804d5918e74deb1b8e8d
AdaChenScience <chengchenc96@gmail.com>
2024-03-12 22:32:08 +0800

    更新

---
+++ L01/ex_12.py
@@ -0,0 +1,26 @@
+def cal(n):
+    if n==1:
+        return 1
+    if n<1:
+        l=n
+        r=1
+    else:
+        l=1
+        r=n
+    while r-l>0.0000001:
+        mid=(l+r)/2
+        if mid*mid*mid==n:
+            return mid
+        elif mid*mid*mid<n:
+            l=mid
+        else:
+            r=mid
+    return (l+r)/2
+
+y=float(input('请输入y:'))
+flag=0 if y>0 else 1
+<<<<<<< HEAD
+print('%f的立方根约为%f'%(y,(-1)**flag*cal(abs(y))))
+=======
+print('%f的立方根为%f'%((-1)**flag*y,(-1)**flag*cal(abs(y))))
+>>>>>>> c418751 (finish L01)
